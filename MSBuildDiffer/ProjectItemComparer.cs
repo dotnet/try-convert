33d0fbb0e04d0a917d915c213a93a0659dac1d98
Srivatsn Narayanan <srivatsn@microsoft.com>
2017-02-16 00:30:57 -0800

    Add project files.

---
+++ MSBuildDiffer/ProjectItemComparer.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using Microsoft.Build.Evaluation;
+
+namespace MSBuildDiffer
+{
+    public class ProjectItemComparer : IEqualityComparer<ProjectItem>
+    {
+        private ProjectItemComparer() { }
+
+        public static ProjectItemComparer Instance = new ProjectItemComparer();
+
+        public bool Equals(ProjectItem x, ProjectItem y)
+        {
+            return x.ItemType == y.ItemType && x.EvaluatedInclude == y.EvaluatedInclude;
+        }
+
+        public int GetHashCode(ProjectItem obj)
+        {
+            return (obj.EvaluatedInclude + obj.ItemType).GetHashCode();
+        }
+    }
+}

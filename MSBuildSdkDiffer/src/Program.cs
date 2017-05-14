23fff3d36533e80e002f672c2e33f1c23220a2bb
Srivatsn Narayanan <srivatsn@microsoft.com>
2017-05-14 03:55:34 -0700

    Refactor some code into a project loader.

--- MSBuildSdkDiffer/src/Program.cs
+++ MSBuildSdkDiffer/src/Program.cs
@@ -1,9 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using Microsoft.Build.Construction;
-using Microsoft.Build.Evaluation;
 using CommandLine;
 
 namespace MSBuildSdkDiffer
@@ -30,80 +25,26 @@ namespace MSBuildSdkDiffer
 
         private static int Run(Options options)
         {
-            string projectPath = Path.GetFullPath(options.ProjectFilePath);
-
-            if (!File.Exists(projectPath))
-            {
-                Console.Error.WriteLine($"The project file '{projectPath}' does not exist or is inaccessible.");
-                return 2;
-            }
-
-            Dictionary<string, string> globalProperties = InitializeGlobalProperties(options);
-
-            var collection = new ProjectCollection(globalProperties);
-            MSBuildProject project = new MSBuildProject(collection.LoadProject(projectPath));
-            
-            Console.WriteLine($"Successfully loaded project file '{projectPath}'.");
-
-            //Stash away names of properties in the file since to create the sdk baseline, we'll modify the project in memory.
-            var rootElement = ProjectRootElement.Open(projectPath);
-            var propertiesInFile = rootElement.Properties.Select(p => p.Name).Distinct().ToList();
-            MSBuildProject sdkBaselineProject = CreateSdkBaselineProject(project, rootElement, globalProperties);
-            Console.WriteLine($"Successfully loaded sdk baseline of project.");
-
+            var projectLoader = new ProjectLoader();
+            projectLoader.LoadProjects(options);
 
             switch (options)
             {
                 case LogOptions opt:
-                    project.LogProjectProperties(opt.CurrentProjectLogPath);
-                    sdkBaselineProject.LogProjectProperties(opt.SdkBaseLineProjectLogPath);
+                    projectLoader.Project.LogProjectProperties(opt.CurrentProjectLogPath);
+                    projectLoader.SdkBaselineProject.LogProjectProperties(opt.SdkBaseLineProjectLogPath);
                     break;
                 case DiffOptions opt:
-                    var differ = new Differ(project, propertiesInFile, sdkBaselineProject);
+                    var differ = new Differ(projectLoader.Project, projectLoader.PropertiesInFile, projectLoader.SdkBaselineProject);
                     differ.GenerateReport(opt.DiffReportPath);
                     break;
                 case ConvertOptions opt:
-                    var converter = new Converter(project, propertiesInFile, sdkBaselineProject, rootElement);
+                    var converter = new Converter(projectLoader.Project, projectLoader.PropertiesInFile, projectLoader.SdkBaselineProject, projectLoader.RootElement);
                     converter.GenerateProjectFile(opt.OutputProjectPath);
                     break;
             }
-            
+
             return 0;
-        }
-
-        private static Dictionary<string, string> InitializeGlobalProperties(Options options)
-        {
-            var globalProperties = new Dictionary<string, string>();
-            if (!string.IsNullOrEmpty(options.RoslynTargetsPath))
-            {
-                globalProperties.Add("RoslynTargetsPath", options.RoslynTargetsPath);
-            }
-
-            if (!string.IsNullOrEmpty(options.MSBuildSdksPath))
-            {
-                globalProperties.Add("MSBuildSDKsPath", options.MSBuildSdksPath);
-            }
-
-            return globalProperties;
-        }
-
-        /// <summary>
-        /// Clear out the project's construction model and add a simple SDK-based project to get a baseline.
-        /// We need to use the same name as the original csproj and same path so that all the default that derive
-        /// from name\path get the right values (there are a lot of them).
-        /// </summary>
-        private static MSBuildProject CreateSdkBaselineProject(MSBuildProject project, ProjectRootElement rootElement, IDictionary<string, string> globalProperties)
-        {
-            rootElement.RemoveAllChildren();
-            rootElement.Sdk = "Microsoft.NET.Sdk";
-            var propGroup = rootElement.AddPropertyGroup();
-            propGroup.AddProperty("TargetFramework", project.GetTargetFramework());
-            propGroup.AddProperty("OutputType", project.GetPropertyValue("OutputType") ?? throw new InvalidOperationException("OutputType is not set!"));
-
-            // Create a new collection because a project with this name has already been loaded into the global collection.
-            var pc = new ProjectCollection(globalProperties);
-            var newProjectModel = new MSBuildProject(new Project(rootElement, null, "15.0", pc));
-            return newProjectModel;
         }
     }
 }
---
+++ MSBuildSdkDiffer/src/ProjectLoader.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using Microsoft.Build.Construction;
+using Microsoft.Build.Evaluation;
+
+namespace MSBuildSdkDiffer
+{
+    internal class ProjectLoader
+    {
+        public MSBuildProject Project { get; private set; }
+        public MSBuildProject SdkBaselineProject { get; private set; }
+        public List<string> PropertiesInFile { get; private set; }
+        public ProjectRootElement RootElement { get; private set; }
+
+        public void LoadProjects(Options options)
+        {
+            string projectPath = Path.GetFullPath(options.ProjectFilePath);
+
+            if (!File.Exists(projectPath))
+            {
+                Console.Error.WriteLine($"The project file '{projectPath}' does not exist or is inaccessible.");
+                return;
+            }
+
+            Dictionary<string, string> globalProperties = InitializeGlobalProperties(options);
+
+            var collection = new ProjectCollection(globalProperties);
+            Project = new MSBuildProject(collection.LoadProject(projectPath));
+            Console.WriteLine($"Successfully loaded project file '{projectPath}'.");
+
+            //Stash away names of properties in the file since to create the sdk baseline, we'll modify the project in memory.
+            RootElement = ProjectRootElement.Open(projectPath);
+            PropertiesInFile = RootElement.Properties.Select(p => p.Name).Distinct().ToList();
+            SdkBaselineProject = CreateSdkBaselineProject(Project, RootElement, globalProperties);
+            Console.WriteLine($"Successfully loaded sdk baseline of project.");
+        }
+
+        private static Dictionary<string, string> InitializeGlobalProperties(Options options)
+        {
+            var globalProperties = new Dictionary<string, string>();
+            if (!string.IsNullOrEmpty(options.RoslynTargetsPath))
+            {
+                globalProperties.Add("RoslynTargetsPath", options.RoslynTargetsPath);
+            }
+
+            if (!string.IsNullOrEmpty(options.MSBuildSdksPath))
+            {
+                globalProperties.Add("MSBuildSDKsPath", options.MSBuildSdksPath);
+            }
+
+            return globalProperties;
+        }
+
+        /// <summary>
+        /// Clear out the project's construction model and add a simple SDK-based project to get a baseline.
+        /// We need to use the same name as the original csproj and same path so that all the default that derive
+        /// from name\path get the right values (there are a lot of them).
+        /// </summary>
+        private static MSBuildProject CreateSdkBaselineProject(MSBuildProject project, ProjectRootElement rootElement, IDictionary<string, string> globalProperties)
+        {
+            rootElement.RemoveAllChildren();
+            rootElement.Sdk = "Microsoft.NET.Sdk";
+            var propGroup = rootElement.AddPropertyGroup();
+            propGroup.AddProperty("TargetFramework", project.GetTargetFramework());
+            propGroup.AddProperty("OutputType", project.GetPropertyValue("OutputType") ?? throw new InvalidOperationException("OutputType is not set!"));
+
+            // Create a new collection because a project with this name has already been loaded into the global collection.
+            var pc = new ProjectCollection(globalProperties);
+            var newProjectModel = new MSBuildProject(new Project(rootElement, null, "15.0", pc));
+            return newProjectModel;
+        }
+
+    }
+}

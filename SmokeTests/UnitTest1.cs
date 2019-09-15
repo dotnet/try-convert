6252decb9060836966ba60cca8171d3517f13480
Phillip Carter <phcart@microsoft.com>
2019-09-15 14:29:01 -0700

    Add smoke testing harness

---
+++ SmokeTests/UnitTest1.cs
@@ -0,0 +1,25 @@
+using Xunit;
+
+namespace SmokeTests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void ConvertsLegacyFSharpConsole()
+        {
+            // TODO
+        }
+
+        [Fact]
+        public void ConvertsWpfFrameworkTemplate()
+        {
+            // TODO
+        }
+
+        [Fact]
+        public void ConvertsWinformsFrameworkTemplate()
+        {
+            // TODO
+        }
+    }
+}

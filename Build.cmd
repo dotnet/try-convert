44fe65b68dc17f03330371b28b76b4deb8e7cb29
Jonathon Marolf <jmarolf@microsoft.com>
2019-09-18 14:11:16 -0700

    produce nuget package on build

--- Build.cmd
+++ Build.cmd
@@ -1,3 +1,3 @@
 @echo off
-powershell -ExecutionPolicy ByPass -NoProfile -command "& """%~dp0eng\common\Build.ps1""" -restore -build %*"
+powershell -ExecutionPolicy ByPass -NoProfile -command "& """%~dp0eng\common\Build.ps1""" -restore -build -pack %*"
 exit /b %ErrorLevel%

86ef7ee865baea4c28cd0b54ff6d8082d752aae5
Jonathon Marolf <jmarolf@microsoft.com>
2019-09-26 22:10:43 -0700

    remove and sort usings

--- src/Conversion/Converter.cs
+++ src/Conversion/Converter.cs
@@ -1,14 +1,7 @@
-using Facts;
-
-using Microsoft.Build.Construction;
-
-using MSBuildAbstractions;
-
-using PackageConversion;
+﻿using MSBuildAbstractions;
 
 using System;
 using System.Collections.Immutable;
-using System.IO;
 using System.Linq;
 using System.Xml;
 using System.Xml.Linq;
--- src/Conversion/ProjectRootElementExtensions.cs
+++ src/Conversion/ProjectRootElementExtensions.cs
@@ -1,14 +1,15 @@
-using Facts;
+﻿using Facts;
+
 using Microsoft.Build.Construction;
+
 using MSBuildAbstractions;
+
 using PackageConversion;
+
 using System;
-using System.Collections.Generic;
 using System.Collections.Immutable;
 using System.IO;
 using System.Linq;
-using System.Text;
-using System.Xml.Linq;
 
 namespace Conversion
 {

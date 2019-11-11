471b64c7c219640ded9034a28b364f419cdb274f
Jonathon Marolf <jmarolf@microsoft.com>
2019-11-11 14:16:54 -0800

    remove ununsed usings

--- src/MSBuild.Abstractions/NugetHelpers.cs
+++ src/MSBuild.Abstractions/NugetHelpers.cs
@@ -4,7 +4,6 @@ using System.IO;
 using System.Net.Http;
 using System.Text.Json;
 using System.Threading.Tasks;
-using Microsoft.VisualBasic;
 
 namespace MSBuild.Abstractions
 {
--- src/MSBuild.Conversion.Facts/MSTestFacts.cs
+++ src/MSBuild.Conversion.Facts/MSTestFacts.cs
@@ -1,7 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Collections.Immutable;
-using System.Text;
 
 namespace MSBuild.Conversion.Facts
 {

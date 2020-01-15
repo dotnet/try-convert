a641798e10ccb34a232414e6db9f4aab7e57ff0b
Jonathon Marolf <jmarolf@microsoft.com>
2020-01-15 10:18:06 -0800

    fix up smoke tests

---
+++ tests/end-to-end/Smoke.Tests/Utilities/MSBuildFixture.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Threading;
+
+using MSBuild.Abstractions;
+
+namespace Smoke.Tests.Utilities
+{
+    /// <summary>
+    /// This test fixture ensures that MSBuild is loaded.
+    /// </summary>
+    public class MSBuildFixture : IDisposable
+    {
+        private static int _registered = 0;
+
+        public void RegisterInstance()
+        {
+            if (Interlocked.Exchange(ref _registered, 1) == 0)
+            {
+                MSBuildHelpers.HookAssemblyResolveForMSBuild();
+            }
+        }
+
+        public void Dispose()
+        {
+        }
+    }
+}

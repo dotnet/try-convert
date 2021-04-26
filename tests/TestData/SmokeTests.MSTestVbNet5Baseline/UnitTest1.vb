5c0394edbd360c96838af80c017a3b8bae7482e0
Ken Schlobohm <keschlob@microsoft.com>
2021-04-26 14:20:59 -0500

    removed test noise

--- tests/TestData/SmokeTests.MSTestVbNet5Baseline/UnitTest1.vb
+++
@@ -1,9 +0,0 @@
-﻿Imports System.Text
-Imports Microsoft.VisualStudio.TestTools.UnitTesting
-
-<TestClass()> Public Class UnitTest1
-
-    <TestMethod()> Public Sub TestMethod1()
-    End Sub
-
-End Class

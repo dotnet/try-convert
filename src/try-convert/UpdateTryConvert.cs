2cb3d639cb53a5a4120ce6345c295493acddf197
John Mancini <johnmancini30@gmail.com>
2021-06-08 20:34:53 -0400

    handle non global case

--- src/try-convert/UpdateTryConvert.cs
+++ src/try-convert/UpdateTryConvert.cs
@@ -6,13 +6,37 @@ namespace MSBuild.Conversion
     {
         public static void Update()
         {
+            var global = IsGlobalTool() ? "--global" : string.Empty;
             using (var process = new Process())
             {
                 process.StartInfo.UseShellExecute = true;
                 process.StartInfo.FileName = "dotnet";
-                process.StartInfo.Arguments = "tool update --global try-convert";
+                process.StartInfo.Arguments = $"tool update {global} try-convert";
                 process.Start();
             }
+        }
+
+        private static bool IsGlobalTool()
+        {
+            using (var process = new Process())
+            {
+                process.StartInfo.UseShellExecute = false;
+                process.StartInfo.FileName = "dotnet";
+                process.StartInfo.Arguments = "tool list --global";
+                process.StartInfo.RedirectStandardOutput = true;
+                process.Start();
+
+                while (!process.StandardOutput.EndOfStream)
+                {
+                    var line = process.StandardOutput.ReadLine();
+                    if (line?.Contains("try-convert") ?? false)
+                    {
+                        return true;
+                    }
+                }
+            }
+
+            return false;
         }
     }
 }
